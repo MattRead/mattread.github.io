debb833017ee2370d878e0e1e4ba28e4e74aef42
Matt Read <matt@mattread.com>
2010-09-12 16:58:59 -0400

    change syntax style

--- compress.sh
+++ compress.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "compressing CSS"
+cd css/
 cat tables.css syntax.css style.css > screen.css
 yui-compressor --type css screen.css > screen-min.css
+echo "compress CSS to screen-min.css"
