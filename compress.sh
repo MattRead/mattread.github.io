4314443620134e7e530395f8ea2c087caa4f59ff
Matt Read <matt@mattread.com>
2010-09-13 22:26:54 -0400

    deployment: compress CSS and JS

--- compress.sh
+++ compress.sh
@@ -2,7 +2,7 @@
 
 echo "compressing CSS"
 cd css/
-cat tables.css syntax.css style.css > screen.css
+cat tables.css syntax.css style.css fonts/dejavu_serif/stylesheet.css > screen.css
 yui-compressor --type css screen.css > screen-min.css
 cd ..
 echo "compressed CSS to screen-min.css"
