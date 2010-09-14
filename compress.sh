fa98d8dbff63f2619e9eb9161f344dd7711460e1
Matt Read <matt@mattread.com>
2010-09-13 22:35:09 -0400

    use less indent on ul,ol,blockquote

--- compress.sh
+++ compress.sh
@@ -2,7 +2,8 @@
 
 echo "compressing CSS"
 cd css/
-cat tables.css syntax.css style.css fonts/dejavu_serif/stylesheet.css > screen.css
-yui-compressor --type css screen.css > screen-min.css
+cat tables.css syntax.css screen.css fonts/dejavu_serif/stylesheet.css > screen.tmp.css
+yui-compressor --type css screen.tmp.css > screen-min.css
+rm screen.tmp.css
 cd ..
 echo "compressed CSS to screen-min.css"
