7485c653d213ed61da04c219769238e56a33f3bf
Matt Read <matt@mattread.com>
2010-09-14 01:03:12 -0400

    deployment: compress CSS and JS

--- js/site.js
+++ js/site.js
@@ -1,1 +1,25 @@
 // this is a placeholder for now
+
+var populate_dents = function(tag) {
+    $.ajax({
+        url: "http://identi.ca/api/statusnet/tags/timeline/" + tag + ".json",
+        dataType: 'json',
+        success: function(data){
+            if (data.length > 0) {
+                $('.related').before('<h2>Dents  in Reply to This Post</h2><p>You can reply to' +
+                                     'this post on identi.ca using the has tag <a' +
+                                     'href="http://identi.ca/tag/' + tag + '">#' + tag +
+                                     '</a></p><ol class="dents"></ol>');
+                 $('.dents').css('font-size', '.9em');
+                var dent = [];
+                for (i in data) {
+                    dent = data[i];
+                    var date = dent.created_at.replace(/\d\d:\d\d:\d\d \+0000/g, '');
+                    $('.dents').append('<li><a href="http://identi.ca/notice/' + dent.id + '">' +
+                                        dent.user.name + "</a> " + dent.statusnet_html +
+                                        '<br>@ <time>' + date + '</time></li>');
+                }
+            }
+        }
+    });
+};
